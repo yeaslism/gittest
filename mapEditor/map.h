db14fa819cf16693b15f8f9cc1b3a2d57cb886d2
yeaslism <yeaslism@google.com>
2016-07-12 17:53:18 +0900

    ...

--- mapEditor/map.h
+++ mapEditor/map.h
@@ -21,6 +21,8 @@ int map_save(_S_MAP_OBJECT *pObj, char *filename);
 void map_dump(_S_MAP_OBJECT *pObj,char *pTile_pal);
 void map_new(_S_MAP_OBJECT *pObj,int nWidth, int nHeight);
 void map_PutTile(_S_MAP_OBJECT *pObj, int x,int y,int nTileIndex);
+
+void map_drawTile(_S_MAP_OBJECT *pObj,int posx,int posy,_S_MAP_OBJECT *pTarget);
 void map_drawTile_mirror_h(_S_MAP_OBJECT *pObj,int posx,int posy,_S_MAP_OBJECT *pTarget);
 void map_drawTile_mirror_v(_S_MAP_OBJECT *pObj,int posx,int posy,_S_MAP_OBJECT *pTarget);
 
