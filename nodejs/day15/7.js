ed26b3b77e16ec009e3ae90030fd9663c94b0926
yeaslism <yeaslism@google.com>
2016-07-18 17:43:45 +0900

    ...

---
+++ nodejs/day15/7.js
@@ -0,0 +1,12 @@
+/**
+ * Created by KICT-17 on 2016-07-18.
+ */
+
+function change(a) {
+    a[0] = 9;
+}
+
+var a = [3,2,1];
+change(a);
+
+console.log(a[0]);
---
+++ nodejs/day15/8.js
@@ -0,0 +1,12 @@
+/**
+ * Created by KICT-17 on 2016-07-18.
+ */
+
+function foo() {
+    console.log("it is foo");
+
+}
+
+var bar = foo;
+
+bar();
