c0258c82cbdf477b62aa34efe13275e920ae5e6e
yeaslism <yeaslism@google.com>
2016-07-18 16:17:45 +0900

    ...

--- day15/alien.h
+++ day15/alien.h
@@ -12,7 +12,11 @@ typedef struct _S_ALIEN_OBJECT {
 	double m_fCenterX;
 	double m_fCenterY;
 
+	double m_fSpeed;
+
 	_S_MAP_OBJECT *m_pBody;
+	_S_BULLET_OBJECT *m_pBullet;
+
 	void (*pfApply)(struct _S_ALIEN_OBJECT *,double);
 	void (*pfDraw)(struct _S_ALIEN_OBJECT *,_S_MAP_OBJECT *);
 
--- day15/ex5.c
+++ day15/ex5.c
@@ -11,6 +11,7 @@
 #include "../engine/engine2d.h"
 #include "../mapEditor/map.h"
 
+#include "bullet.h"
 #include "alien.h"
 
 struct timespec work_timer;
@@ -20,7 +21,10 @@ int bLoop = 1;
 _S_MAP_OBJECT gScreenBuf[2];
 
 _S_MAP_OBJECT gAlienModel;
+_S_MAP_OBJECT gPlasmaModel;
+
 _S_ALIEN_OBJECT gTestAlienObject;
+_S_BULLET_OBJECT gTestBulletObject;
 
 int main()
 {
@@ -34,14 +38,22 @@ int main()
 	map_init(&gAlienModel);
 	map_load(&gAlienModel,"alien.dat");
 
+	map_init(&gPlasmaModel);
+	map_load(&gPlasmaModel,"plasma.dat");
+
 	alien_init(&gTestAlienObject,&gAlienModel);
+	bullet_init(&gTestBulletObject,0,0,0,&gPlasmaModel);
+
+	gTestAlienObject.m_pBullet = &gTestBulletObject;
+
+	gTestAlienObject.m_fXpos = 5;
+	gTestAlienObject.m_fYpos = 5;
+	gTestAlienObject.m_nFSM = 1;
 
 	set_conio_terminal_mode();
 	acc_tick=last_tick=0;
 
 	system("clear");
-
-	gTestAlienObject.m_nFSM = 1;
 
 	while(bLoop) {
 		//타이밍처리
@@ -59,6 +71,9 @@ int main()
 			}
 		}
 	
+		gTestAlienObject.pfApply(&gTestAlienObject,delta_tick);
+		gTestBulletObject.pfApply(&gTestBulletObject,delta_tick);
+
 		//타이밍 계산
 		acc_tick += delta_tick;
 		if(acc_tick > 0.1) {
@@ -66,6 +81,7 @@ int main()
 			map_drawTile(&gScreenBuf[0],0,0,&gScreenBuf[1]);
 
 			gTestAlienObject.pfDraw(&gTestAlienObject,&gScreenBuf[1]);
+			gTestBulletObject.pfDraw(&gTestBulletObject,&gScreenBuf[1]);
 
 			map_dump(&gScreenBuf[1],Default_TilePalette);
 			acc_tick = 0;
